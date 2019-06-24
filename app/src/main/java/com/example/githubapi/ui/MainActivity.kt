1f6ef69536d7fd206c786836147b9e2dfa01413c
Fahri MacBook <fahri-can@web.de>
2019-06-24 23:15:41 +0200

    Clear all disposables.

--- app/src/main/java/com/example/githubapi/ui/MainActivity.kt
+++ app/src/main/java/com/example/githubapi/ui/MainActivity.kt
@@ -3,19 +3,25 @@ package com.example.githubapi.ui
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.widget.SwipeRefreshLayout
+import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.LinearLayoutManager
 import com.example.githubapi.R
 import com.example.githubapi.adapter.RepositoryAdapter
 import com.example.githubapi.api_endpoint.GitHubRepositories
 import com.example.githubapi.model.Repository
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import io.reactivex.Observable
 import io.reactivex.disposables.CompositeDisposable
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity(), SwipeRefreshLayout.OnRefreshListener {
 
     private val ENDPOINT_URL by lazy { "https://api.github.com/" }
     private lateinit var gitHubRepositories: GitHubRepositories
-    private lateinit var repositoryAdapter: RepositoryAdapter
     private lateinit var repositoriesList: ArrayList<Repository>
+    private lateinit var repositoryAdapter: RepositoryAdapter
     // RxJava related fields
     private lateinit var repositoryObservable: Observable<Repository>
     private lateinit var compositeDisposable: CompositeDisposable
@@ -23,9 +29,37 @@ class MainActivity : AppCompatActivity(), SwipeRefreshLayout.OnRefreshListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Network request
+        val retrofit: Retrofit = generateRetrofitGsonBuilder()
+        gitHubRepositories = retrofit.create(GitHubRepositories::class.java)
+        swipe_refresh.setOnRefreshListener(this)
+        swipe_refresh.setColorSchemeResources(R.color.colorAccent)
+        repositoriesList = ArrayList()
+        repositoryAdapter = RepositoryAdapter(repositoriesList)
+        compositeDisposable = CompositeDisposable()
+        recycler_view.setHasFixedSize(true)
+        //TODO: set number of items to 25 per page
+        recycler_view.layoutManager = LinearLayoutManager(this)
+        recycler_view.itemAnimator = DefaultItemAnimator()
+        recycler_view.adapter = repositoryAdapter
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.clear()
     }
 
     override fun onRefresh() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private fun generateRetrofitGsonBuilder(): Retrofit {
+
+        return Retrofit.Builder()
+            .baseUrl(ENDPOINT_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
     }
 }
