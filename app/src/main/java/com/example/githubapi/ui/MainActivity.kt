c91c5acac84e408653a7732762dc2f4fb0a6e81f
Fahri MacBook <fahri-can@web.de>
2019-06-24 21:44:03 +0200

    Add swipe refresh.

--- app/src/main/java/com/example/githubapi/ui/MainActivity.kt
+++ app/src/main/java/com/example/githubapi/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.githubapi.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.widget.SwipeRefreshLayout
 import com.example.githubapi.R
 import com.example.githubapi.adapter.RepositoryAdapter
 import com.example.githubapi.api_endpoint.GitHubRepositories
@@ -9,7 +10,7 @@ import com.example.githubapi.model.Repository
 import io.reactivex.Observable
 import io.reactivex.disposables.CompositeDisposable
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SwipeRefreshLayout.OnRefreshListener {
 
     private val ENDPOINT_URL by lazy { "https://api.github.com/" }
     private lateinit var gitHubRepositories: GitHubRepositories
@@ -22,5 +23,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onRefresh() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
