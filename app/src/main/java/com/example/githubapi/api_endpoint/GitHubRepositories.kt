f7a4f8fd2959aaaa183739041d0b9c3858cba6e5
Fahri MacBook <fahri-can@web.de>
2019-06-25 00:21:52 +0200

    Change method in interface for fetching repos.

--- app/src/main/java/com/example/githubapi/api_endpoint/GitHubRepositories.kt
+++ app/src/main/java/com/example/githubapi/api_endpoint/GitHubRepositories.kt
@@ -2,10 +2,15 @@ package com.example.githubapi.api_endpoint
 
 import com.example.githubapi.model.Repository
 import io.reactivex.Observable
+import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Url
 
 interface GitHubRepositories {
+/*
+    @GET()
+    fun fetchAllPublicRepositories(@Url url: String): Observable<List<Repository>>*/
 
-    @GET("repositories")
-    fun fetchAllPublicRepositories(): Observable<Repository>
+    @GET
+    fun fetchAllPublicRepositories(@Url url: String): Call<List<Repository>>
 }
