26243eaee02002777d29290585153bbb10f32177
Fahri MacBook <fahri-can@web.de>
2019-06-24 20:47:02 +0200

    Start implement view holder and adapter.

---
+++ app/src/main/java/com/example/githubapi/adapter/GitHubRepositoryAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.githubapi.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+class GitHubRepositoryAdapter : RecyclerView.Adapter<GitHubRepositoryAdapter.GitHubRepositoryViewHolder>() {
+
+    override fun onBindViewHolder(p0: GitHubRepositoryViewHolder, p1: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): GitHubRepositoryViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+
+    inner class GitHubRepositoryViewHolder(private val view: View) : RecyclerView.ViewHolder(view)
+}
