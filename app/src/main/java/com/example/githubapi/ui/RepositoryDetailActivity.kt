94239cede8f905164e00f67246698f6fd1704117
Fahri MacBook <fahri-can@web.de>
2019-06-25 01:47:33 +0200

    Add necessary layout and class for repository details.

---
+++ app/src/main/java/com/example/githubapi/ui/RepositoryDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.example.githubapi.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.githubapi.R
+
+class RepositoryDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_repository_detail)
+    }
+}
