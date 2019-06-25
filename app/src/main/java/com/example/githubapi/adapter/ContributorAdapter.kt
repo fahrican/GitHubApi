6ee7e19f8bdc6bf3567f43b98c8ae12c3c3dfc62
Fahri MacBook <fahri-can@web.de>
2019-06-26 01:32:33 +0200

    Start with ViewHolder and Adapter for contributor.

---
+++ app/src/main/java/com/example/githubapi/adapter/ContributorAdapter.kt
@@ -0,0 +1,16 @@
+package com.example.githubapi.adapter
+
+import android.support.v7.widget.CardView
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.widget.TextView
+import kotlinx.android.synthetic.main.item_contributor.view.*
+
+class ContributorAdapter {
+
+    inner class ContributorViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+
+        val cardView: CardView by lazy { view.contributorCardView }
+        val login: TextView by lazy { view.loginTextView }
+    }
+}
