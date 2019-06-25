4be4c61d5fe6cde3f8e2c9439fdb9cc7590c3d3d
Fahri MacBook <fahri-can@web.de>
2019-06-25 21:57:16 +0200

    Test data passing with intent.

--- app/src/main/java/com/example/githubapi/ui/RepositoryDetailActivity.kt
+++ app/src/main/java/com/example/githubapi/ui/RepositoryDetailActivity.kt
@@ -3,11 +3,19 @@ package com.example.githubapi.ui
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.githubapi.R
+import com.example.githubapi.adapter.RepositoryAdapter
+import kotlinx.android.synthetic.main.activity_repository_detail.*
 
 class RepositoryDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_repository_detail)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        full_name.text = intent.getStringExtra(RepositoryAdapter.FULL_NAME)
     }
 }
