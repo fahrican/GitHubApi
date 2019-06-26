5c347d8f3f3526c57aa8aa04fff27280ee65fb8b
Fahri MacBook <fahri-can@web.de>
2019-06-26 21:08:52 +0200

    Finish implement activity.

--- app/src/main/java/com/example/githubapi/ui/ContributorActivity.kt
+++ app/src/main/java/com/example/githubapi/ui/ContributorActivity.kt
@@ -40,10 +40,89 @@ class ContributorActivity : AppCompatActivity() {
         supportActionBar?.title = "List of all Contributors"
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowHomeEnabled(true)
+        fullNameFromIntent = intent.getStringExtra(RepositoryAdapter.FULL_NAME)
+        //Network request
+        val retrofit: Retrofit = generateRetrofitGsonBuilder()
+        containsLoginAndName = ArrayList()
+        //Get {login}/{repository_name}
+        containsLoginAndName = fullNameFromIntent.split("/")
+        gitHubRepositories = retrofit.create(GitHubRepositories::class.java)
+        contributorsList = ArrayList()
+        contributorAdapter = ContributorAdapter(contributorsList)
+        compositeDisposable = CompositeDisposable()
+        //list of contributors
+        contributorRecyclerView.setHasFixedSize(true)
+        contributorRecyclerView.layoutManager = LinearLayoutManager(this)
+        contributorRecyclerView.itemAnimator = DefaultItemAnimator()
+        contributorRecyclerView.adapter = contributorAdapter
     }
 
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
+    }
+
+    override fun onStart() {
+        super.onStart()
+        fetchForContributors()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.clear()
+    }
+
+    private fun fetchForContributors() {
+        repositoryObservable =
+            gitHubRepositories.fetchContributorsList(containsLoginAndName.get(0), containsLoginAndName.get(1))
+        subscribeObservableOfContributor()
+    }
+
+    private fun subscribeObservableOfContributor() {
+        contributorsList.clear()
+        compositeDisposable.add(
+            repositoryObservable.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(createContributorObserver())
+        )
+    }
+
+    private fun createContributorObserver(): DisposableObserver<ArrayList<Contributor>> {
+        return object : DisposableObserver<ArrayList<Contributor>>() {
+
+            override fun onNext(contributors: ArrayList<Contributor>) {
+                if (!contributorsList.equals(contributors)) {
+                    contributorsList = contributors
+                }
+            }
+
+            override fun onComplete() {
+                showArticlesOnRecyclerView()
+            }
+
+
+            override fun onError(e: Throwable) {
+                Log.e("createContributorObserv", "Contributor error: ${e.message}")
+            }
+
+        }
+    }
+
+    private fun showArticlesOnRecyclerView() {
+        if (contributorsList.size > 0) {
+            contributorRecyclerView.visibility = View.VISIBLE
+            contributorAdapter.setContributors(contributorsList)
+        } else {
+            contributorRecyclerView.visibility = View.GONE
+        }
+    }
+
+    private fun generateRetrofitGsonBuilder(): Retrofit {
+
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
     }
 }
