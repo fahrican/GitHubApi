5aec56f98f4985d46ed79878cd36068d1282c50d
Fahri MacBook <fahri-can@web.de>
2019-06-30 00:19:44 +0200

    Successfully ad pagination of 25 items with Retrofit Call.

--- app/src/main/java/com/example/githubapi/ui/MainActivity.kt
+++ app/src/main/java/com/example/githubapi/ui/MainActivity.kt
@@ -2,8 +2,6 @@ package com.example.githubapi.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v4.widget.SwipeRefreshLayout
-import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.Log
@@ -14,11 +12,11 @@ import com.example.githubapi.api_endpoint.GitHubRepositories
 import com.example.githubapi.api_endpoint.RetrofitInstance
 import com.example.githubapi.model.Repository
 import io.reactivex.Observable
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.observers.DisposableObserver
-import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,14 +25,14 @@ class MainActivity : AppCompatActivity() {
     // RxJava related fields
     private lateinit var repositoryObservable: Observable<ArrayList<Repository>>
     private lateinit var compositeDisposable: CompositeDisposable
-
+    //Items for OnScrollListener()
     private lateinit var layoutManager: RecyclerView.LayoutManager
     var visibleItemCount = 0
     var pastVisibleItemCount = 0
     var loading = false
     var pageId = 1
     var totalItemCount = 0
-    private val PAGE_SIZE = 10
+    private val PAGE_SIZE = 25
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,14 +50,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        fetchForRepositories()
+        fetchAllPublicRepositories()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         compositeDisposable.clear()
     }
-
+/*
     private fun fetchForRepositories() {
         progressBar.visibility = View.VISIBLE
         val gitHubRepositories: GitHubRepositories = RetrofitInstance.getEndPoint()
@@ -114,37 +112,84 @@ class MainActivity : AppCompatActivity() {
             mainretryFetchButton.setOnClickListener { fetchForRepositories() }
         }
     }
+*/
+
+    private fun fetchAllPublicRepositories() {
+        progressBar.visibility = View.VISIBLE
+        val gitHubRepositories: GitHubRepositories = RetrofitInstance.getEndPoint()
+        val call: Call<ArrayList<Repository>> = gitHubRepositories.fetchAllPublicRepositories()
+        call.enqueue(generateRepositoryFetch())
+    }
+
+    private fun generateRepositoryFetch(): Callback<ArrayList<Repository>> {
+        return object : Callback<ArrayList<Repository>> {
+
+            override fun onFailure(call: Call<ArrayList<Repository>>, t: Throwable) {
+                mainEmptyText.text = "Something went wrong!\n${t.message}"
+            }
+
+            override fun onResponse(call: Call<ArrayList<Repository>>, response: Response<ArrayList<Repository>>) {
+                if (!response.isSuccessful) {
+                    mainEmptyText.text = "code: ${response.code()}"
+                    return
+                }
+                showArticlesOnRecyclerView(response)
+            }
+        }
+    }
+
+    private fun showArticlesOnRecyclerView(repos: Response<ArrayList<Repository>>) {
+        val temp: ArrayList<Repository>? = repos.body()
+        if (temp != null && temp.size > 0) {
+            progressBar.visibility = View.GONE
+            loading = true
+            setUpAdapter(temp)
+            mainEmptyText.visibility = View.GONE
+            mainretryFetchButton.visibility = View.GONE
+            mainRecyclerView.visibility = View.VISIBLE
+        } else {
+            mainRecyclerView.visibility = View.GONE
+            progressBar.visibility = View.GONE
+            mainEmptyText.visibility = View.VISIBLE
+            mainretryFetchButton.visibility = View.VISIBLE
+            mainretryFetchButton.setOnClickListener { fetchAllPublicRepositories() }
+        }
+    }
 
     private fun setUpAdapter(repos: ArrayList<Repository>) {
-        if (repositoriesList.size == 0){
+        if (repositoriesList.size == 0) {
             repositoriesList = repos
             repositoryAdapter = RepositoryAdapter(repositoriesList)
             mainRecyclerView.adapter = repositoryAdapter
-        }
-        else {
-            var currentPosition = (mainRecyclerView.layoutManager as LinearLayoutManager).findLastVisibleItemPosition()
+        } else {
+            val currentPosition = (mainRecyclerView.layoutManager as LinearLayoutManager).findLastVisibleItemPosition()
             repositoriesList.addAll(repos)
             repositoryAdapter.notifyDataSetChanged()
             mainRecyclerView.scrollToPosition(currentPosition)
         }
-        mainRecyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener(){
+        mainRecyclerView.addOnScrollListener(checkNumberOfScrolledItems())
+    }
 
+    private fun checkNumberOfScrolledItems(): RecyclerView.OnScrollListener {
+        return object : RecyclerView.OnScrollListener() {
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-
-                if (dy > 0){
+                if (dy > 0) {
                     visibleItemCount = layoutManager.childCount
                     totalItemCount = layoutManager.itemCount
-                    pastVisibleItemCount = (mainRecyclerView.layoutManager as LinearLayoutManager).findLastVisibleItemPosition()
-                    if (loading){
-                        if ((visibleItemCount + pastVisibleItemCount) >= 8){
+                    pastVisibleItemCount =
+                        (mainRecyclerView.layoutManager as LinearLayoutManager).findLastVisibleItemPosition()
+                    if (loading) {
+                        if ((visibleItemCount + pastVisibleItemCount) >= 8 && totalItemCount >= PAGE_SIZE) {
                             loading = false
                             pageId++
-                            fetchForRepositories()
+                            Log.v("page", "${pageId}")
+                            progressBar.visibility = View.GONE
+                            fetchAllPublicRepositories()
                         }
                     }
                 }
             }
 
-        })
+        }
     }
 }
